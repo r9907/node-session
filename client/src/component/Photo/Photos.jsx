15e6db9a06ab302947fccfdc33bc3dbb0d65a609
r9907 <165926827+r9907@users.noreply.github.com>
2024-04-07 01:30:17 +0300

    second change

--- client/src/component/Photo/Photos.jsx
+++ client/src/component/Photo/Photos.jsx
@@ -3,6 +3,7 @@ const Photos=()=>{
         <>
         <body style={{textAlign:'center'}}>
         <h1 style={{fontFamily:'Comic Sans MS'}}>אין תמונות זמינות!!! נסב שוב</h1>
+        <h2>שורה חדשה...</h2>
         </body>
         </>
     )
